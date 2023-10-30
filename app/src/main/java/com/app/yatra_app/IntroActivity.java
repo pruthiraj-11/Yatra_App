47ddb66eb2e8cc10e6b9cae82727338793934cb2
Pruthiraj Routaray <113031912+pruthiraj-74@users.noreply.github.com>
2023-10-30 16:26:02 +0530

    AIRBNBC

--- app/src/main/java/com/app/yatra_app/IntroActivity.java
+++ app/src/main/java/com/app/yatra_app/IntroActivity.java
@@ -1,7 +1,9 @@
 package com.app.yatra_app;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,6 +16,10 @@ public class IntroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding=ActivityIntroBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.KITKAT){
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
 
         binding.nextbtn.setOnClickListener(view -> {
             startActivity(new Intent(this, MainActivity.class));
